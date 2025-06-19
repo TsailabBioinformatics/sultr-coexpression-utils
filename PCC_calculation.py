971245269d44ee9ea2b8c937e37bdb3318d10464
Chen Hsieh <70214580+ChenHsieh@users.noreply.github.com>
2025-06-19 16:11:22 -0400

    Update PCC_calculation.py

--- PCC_calculation.py
+++ PCC_calculation.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Load expression matrix with genes as rows, samples as columns
-df = pd.read_csv('./data/dynamic_expression_matrix_TPM5_CV50_stem.csv', index_col=0)
+df = pd.read_csv('data/dynamic_expression_matrix_TPM5_CV50_stem.csv', index_col=0)
 
 # Transpose to have genes as columns for correlation calculation
 df = df.T
@@ -12,13 +12,12 @@ goi_dict = {
     "SULTR3.4a": "PtXaTreH.03G041400"
 }
 goi = list(goi_dict.values())
-print(df[goi].head())  # Visual check
+print(df.loc[goi].head())  # Visual check
 
-# Transpose back if needed (though not required for correlation here)
-# df = df.T
+df = df.T
 
 # Compute Pearson correlation coefficient matrix
 pcc_matrix = df.corr(method='pearson')
 
 # Save PCC matrix to file
-pcc_matrix.to_csv('./data/pcc_matrix.csv')
+pcc_matrix.to_csv('./data/pcc_matrix_174_samples.csv')
